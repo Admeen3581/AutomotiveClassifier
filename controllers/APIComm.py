dc3b129ab83c522c0a7f78351781a3ca804a702d
Adam Long <adam.jacob.long@gmail.com>
2025-12-07 10:18:22 -0600

    Finalize API Comms

--- controllers/APIComm.py
+++ controllers/APIComm.py
@@ -13,6 +13,7 @@ License: MIT - ALL RIGHTS RESERVED
 #Imports
 import os
 import subprocess
+import zipfile
 from dotenv import load_dotenv
 
 #Constants
@@ -59,10 +60,32 @@ def download_dataset():
     except FileNotFoundError:
         print("Error: 'kaggle' command not found. Ensure the Kaggle CLI is installed and in your PATH.")
 
+    #Zip constants
+    zip_filename = DATASET_ID.split('/')[-1] + '.zip'
+    zip_path = os.path.join(DOWNLOAD_DIR, zip_filename)
+
+    if os.path.exists(zip_path):
+        print(f"Unzipping data from {zip_filename}...")
+        os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(DOWNLOAD_DIR)
+
+        print(f"Extraction complete. Files are in: {DOWNLOAD_DIR}")
+
+        # Delete the large ZIP file to save disk space
+        os.remove(zip_path)
+        print(f"Cleaned up the ZIP file.")
+    else:
+        print(f"Could not find the downloaded zip file at {zip_path}. Check for typos in the dataset ID.")
+
 
 def delete_dataset():
     if os.path.exists(DOWNLOAD_DIR):
         print(f"Deleting dataset directory '{DOWNLOAD_DIR}'...")
+        for filepath in os.listdir(DOWNLOAD_DIR):
+            os.remove(DOWNLOAD_DIR + "\{}".format(filepath))
         os.rmdir(DOWNLOAD_DIR)
+        print(f"Deletion of dataset directory '{DOWNLOAD_DIR}' successful.")
     else:
         print(f"Dataset directory '{DOWNLOAD_DIR}' does not exist. Skipping deletion.")
