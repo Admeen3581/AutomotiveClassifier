d964f4b5a99423b08ea8edeaddb1e72a2a61316a
Adam Long <adam.jacob.long@gmail.com>
2025-12-07 14:23:45 -0600

    Add documentation

--- controllers/DatasetFiltering.py
+++ controllers/DatasetFiltering.py
@@ -19,10 +19,21 @@ from controllers.CarMakeData import car_brands
 #Constants
 SOURCE_DIR = Path("./data/stanford_cars/car_data/car_data")
 TARGET_DIR = Path("./data/filtered_cars")
+SHEETS_DIR = Path("./data/stanford_cars/")
 DATA_SPLITS = ["train", "test"]
 
 
 def reorganize_dataset():
+    """
+    Cleans dataset by removing unnecessary files and structuring to fit model requirements.
+
+    Reorganizes a dataset by splitting images into target directories based on their subfolder names,
+    representing car brands. Each car brand folder is grouped and copied to the corresponding target
+    dataset structure while ensuring that only valid car brands are included.
+
+    :raises FileNotFoundError: If the source directory or one of its subdirectories does not exist.
+    :raises OSError: If there is an error creating directories or copying files.
+    """
 
     for split in DATA_SPLITS:
 
@@ -47,3 +58,22 @@ def reorganize_dataset():
 
                 for image_file in folder.glob("*.jpg"):
                     shutil.copy2(image_file, make_dest_dir / image_file.name)
+
+    move_sheets()
+
+def move_sheets():
+    """
+    Moves all CSV sheets from the source directory to the target directory.
+
+    This function iterates through all the files in the directory specified
+    by the global variable ``SHEETS_DIR``, filtering only the files with a
+    ``.csv`` extension. Each matching file is then moved to the directory
+    specified by the global variable ``TARGET_DIR`` using the ``shutil.move``
+    function.
+
+    :raises FileNotFoundError: If the source directory or files do not exist.
+    :raises OSError: For other errors during file moving, such as invalid
+        file paths.
+    """
+    for sheet in SHEETS_DIR.glob("*.csv"):
+        shutil.move(sheet, TARGET_DIR)
