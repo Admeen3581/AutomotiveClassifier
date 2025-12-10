a073ed3588e9b92bc4b6f249fbc7f89f723f58eb
Adam Long <adam.jacob.long@gmail.com>
2025-12-10 08:44:19 -0600

    Add file

---
+++ modelValidation/PrimaryValidation.py
@@ -0,0 +1,23 @@
+"""
+PrimaryValidation.py
+Description: Validates the model.
+
+Contributors:
+[
+    Adam Long <adam.jacob.long@gmail.com>
+]
+
+License: MIT - ALL RIGHTS RESERVED
+"""
+
+
+#Imports
+import os
+
+from modelConstruction.PartDataset import CarPartDataset
+
+
+def validate_model():
+
+
+    test_dataset = CarPartDataset()
