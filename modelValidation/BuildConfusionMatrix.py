c884d8990dd2509e551e3edd192c6e5505443968
Adam Long <adam.jacob.long@gmail.com>
2025-12-10 21:36:55 -0600

    final typo edit

--- modelValidation/BuildConfusionMatrix.py
+++ modelValidation/BuildConfusionMatrix.py
@@ -25,7 +25,7 @@ def build_matrix(all_preds: list, all_labels: list, show_plot: bool = True):
 
     matrix = confusion_matrix(all_labels, all_preds, normalize='true', labels=list(range(NUM_CLASSES)))
 
-    plt.figure(figsize=(18, 18))#Hardcode plot big enough for 40x40
+    plt.figure(figsize=(24, 24))#hardcoded value that fits best
 
     display = ConfusionMatrixDisplay(confusion_matrix=matrix, display_labels=car_brands)
     display.plot(
@@ -34,16 +34,21 @@ def build_matrix(all_preds: list, all_labels: list, show_plot: bool = True):
         values_format='.2f'
     )
 
+    axis = display.ax_
+    axis.tick_params(axis='both', which='major', labelsize=3)
+    for text in axis.texts:
+        text.set_fontsize(2)
+
     plt.title("Confusion Matrix (VMC Test Dataset)")
-    plt.xlabel("Predicted Label")
-    plt.ylabel("True Label")
+    plt.xlabel("Predicted Label").set_fontsize(5)
+    plt.ylabel("True Label").set_fontsize(5)
 
     #Save matrix
     save_path = os.path.join("./output", "ConfusionMatrix.png")
 
     plt.tight_layout()
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    plt.savefig(save_path)
+    plt.savefig(save_path, dpi=200)
     if show_plot:
         plt.show()
 
